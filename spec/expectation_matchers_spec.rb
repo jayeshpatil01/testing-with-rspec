df72eba120d00be4f65dcc6d623c036449fe7188
Jay <jayeshpatil8041@gmail.com>
2024-08-07 12:59:27 +0530

    numeric comparison matchers

--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -60,4 +60,20 @@ describe 'Expectation Matchers' do
       expect(0).not_to be_nil         # nil only, just like #nil?
     end
   end
+
+  describe 'nuemeric comparison matchers' do
+    it 'will match less than/greater than' do
+      expect(10).to be > 9
+      expect(10).to be >= 10
+      expect(10).to be <= 10
+      expect(9).to be < 10
+    end
+
+    it 'will match numeric ranges' do
+      expect(10).to be_between(5, 10).inclusive
+      expect(10).not_to be_between(5, 10).exclusive
+      expect(10).to be_within(1).of(11)
+      expect(5..10).to cover(9)
+    end
+  end
 end
