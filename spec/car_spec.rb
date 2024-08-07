7a84975fda4a1fe08999ef0bb11a71161ae6cef9
Jay <jayeshpatil8041@gmail.com>
2024-08-07 11:50:37 +0530

    skipping examples and working with pending examples

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -3,31 +3,35 @@ require_relative '../car_project/lib/car'
 describe 'Car' do
 
   describe 'attributes' do
-    it "allows reading and writing for :make" do
+    xit "allows reading and writing for :make" do
       car = Car.new
       car.make = "Test"
       expect(car.make).to eq("Test")
     end
 
     it "allows reading and writing for :year" do
+      skip 'Debugging a problem'
       car = Car.new
       car.year = 2005
       expect(car.year).to eq(2005)
     end
 
     it "allows reading and writing for :color" do
+      pending('Debugging a problem')
       car = Car.new
       car.color = 'Red'
-      expect(car.color).to eq('Red')
+      expect(car.color).to eq('Red1')
     end
 
     it "allows reading for wheels" do
       car = Car.new
       expect(car.wheels).to eq(4)
     end
+
+    it "allows writing for :doors"
   end
 
-  describe ".colors" do
+  xdescribe ".colors" do
     it "returs an array of colors" do
       c = ['blue', 'black', 'red', 'green']
       expect(Car.colors).to match_array(c)
