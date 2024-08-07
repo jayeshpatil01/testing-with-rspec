8b188c5734eed313912f40468ca052df04c57390
Jay <jayeshpatil8041@gmail.com>
2024-08-07 13:14:31 +0530

    collection matches - useful while working with arrays, hashes and strings

--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -76,4 +76,44 @@ describe 'Expectation Matchers' do
       expect(5..10).to cover(9)
     end
   end
+
+  describe 'collection mathers' do
+    it 'will match arrays' do
+      array = [1,2,3]
+
+      expect(array).to include(3)
+      expect(array).to include(1,3)
+
+      expect(array).to start_with(1)
+      expect(array).to end_with(3)
+
+      expect(array).to match_array([3,2,1])
+      expect(array).not_to match_array([1,2])
+
+      expect(array).to contain_exactly(3,2,1)       # similar to match_array
+      expect(array).not_to contain_exactly(1,2)     # but use individual args
+    end
+
+    it 'will mtach strings' do
+      string = 'some string'
+
+      expect(string).to include('ring')
+      expect(string).to include('so', 'ring')
+
+      expect(string).to start_with('so')
+      expect(string).to end_with('ing')
+    end
+
+    it 'will match hashes' do
+      hash = {:a => 1, :b => 2, :c => 3}
+
+      expect(hash).to include(:a)
+      expect(hash).to include(:a, :b, :c)
+      
+      expect(hash).to include(:a => 1, :b => 2)
+      expect(hash).to include({:a => 1, :b => 2, :c => 3})
+
+      expect(hash).not_to include({'a' => 1, 'c' => 3}) # as it defaults in ruby symbol key won't match with string key
+    end    
+  end
 end
