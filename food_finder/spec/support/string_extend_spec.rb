3ca74a7414d5c503e2c9532ba50678bb3729d6f2
Jay <jayeshpatil8041@gmail.com>
2024-08-08 21:57:56 +0530

    solution to string-extend class specs challenge

--- food_finder/spec/support/string_extend_spec.rb
+++ food_finder/spec/support/string_extend_spec.rb
@@ -2,31 +2,53 @@ describe 'String' do
 
   describe "#titleize" do
 
-    it "capitalizes each word in a string"
+    it "capitalizes each word in a string" do
+      expect('this is my string'.titleize).to eq('This Is My String')
+    end
     
-    it "works with single-word strings"
+    it "works with single-word strings" do
+      expect('test'.titleize).to eq('Test')
+    end
     
-    it "capitalizes all uppercase strings"
+    it "capitalizes all uppercase strings" do
+      expect('TEST'.capitalize).to eq('Test')
+    end
     
-    it "capitalizes mixed-case strings"
+    it "capitalizes mixed-case strings" do
+      expect('ThIs to BE capITlizEd'.capitalize).to eq('This to be capitlized')
+    end
     
   end
   
   describe '#blank?' do
 
-    it "returns true if string is empty"
+    it "returns true if string is empty" do
+      expect('').to be_blank  # this is a predicate matcher
+      # similar to above is
+      # expect(''.blank?).to be true
+    end
 
-    it "returns true if string contains only spaces"
+    it "returns true if string contains only spaces" do
+      expect('      ').to be_blank
+    end
 
-    it "returns true if string contains only tabs"
-    # Get a tab using a double-quoted string with \t
-    # example: "\t\t\t"
+    it "returns true if string contains only tabs" do
+      # Get a tab using a double-quoted string with \t
+      # example: "\t\t\t"
+      expect("\t\t").to be_blank
+    end
 
-    it "returns true if string contains only spaces and tabs"
+    it "returns true if string contains only spaces and tabs" do
+      expect("\t     \t\t   \t\t\t    ").to be_blank
+    end
     
-    it "returns false if string contains a letter"
+    it "returns false if string contains a letter" do
+      expect("   a     \t\t\t   \t").not_to be_blank
+    end
 
-    it "returns false if string contains a number"
+    it "returns false if string contains a number" do
+      expect("   3  \t").not_to be_blank
+    end
     
   end
   
